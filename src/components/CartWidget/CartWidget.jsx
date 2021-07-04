229fee15ad4851cf55909560facfc51daceac37f
Daniel Castro <daniel.castrov.98@gmail.com>
2021-07-04 16:21:34 -0500

    carpeta de imagenes

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import './CartWidget.css';
-import cart from './shopping-cart.png'
+import cart from '../../imagenes/shopping-cart.png';
 
 const CartWidget = () => {
     return(
